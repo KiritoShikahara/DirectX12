a5b9b6b8e38d3346afc7866670830162d3570764
TheParaDogx <Kiritoshikahara@gmail.com>
2026-04-23 17:52:54 +0900

    pchの作成

---
+++ CoreEngine/src/pch/pch.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include<utility>
+#include<type_traits>
+#include<typeindex>
+#include<typeinfo>
+
+#include<vector>
+#include<array>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+
+#include<string>
+#include<string_view>
+#include<filesystem>
+#include<format>
+
+#include<memory>
+#include<optional>
+#include<variant>
+#include<concepts>
+#include<coroutine>
+#include <chrono>
+
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<tuple>
+#include<span>
+#include<ranges>
+#include<initializer_list>
+
+#include<thread>
+#include<mutex>
+#include<atomic>
+
+#include<iostream>
+#include<sstream>
+#include<fstream>
+
+#include <dxgidebug.h>
+#include<cassert>
+#include<cstdint>
