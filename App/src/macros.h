63e5e8bcf628880f16841b24ce5bd095c66cf0d2
kirito <kiritoshikahara@gmail.com>
2026-06-25 16:34:43 +0900

    DEFAULTのシーンを変更

--- App/src/macros.h
+++ App/src/macros.h
@@ -5,11 +5,12 @@
 
 
 // Scene
+#define TEST_SCENE_NAME  "Test"
 #define TITLE_SCENE_NAME "Title"
 #define MENU_SCENE_NAME "Menu"
 #define GAME_SCENE_NAME "Game"
 
-#define START_SCENE_NAME MENU_SCENE_NAME
+#define START_SCENE_NAME GAME_SCENE_NAME
 
 
 #endif
