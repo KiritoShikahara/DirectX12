9bfc0dace3a981a270b944d92034c9b48622d4a3
4A-11\koyo <a>
2026-07-03 18:00:42 +0900

    リクエスト管理と入力の作成

--- App/src/system/Player/InputSystem/PlayerInputSystem.cpp
+++ App/src/system/Player/InputSystem/PlayerInputSystem.cpp
@@ -3,17 +3,24 @@
 
 #include<Tag/EntityTag.h>
 #include"../MovementSystem/PlayerMovementComponent.h"
+#include"../State/PlayerStateComponent.h"
 
 namespace ecs
 {
 	void PlayerInputSystem::Update(entt::registry& registry, float deltaTime, float rawDeltaTime)
 	{
+		// “ü—Í‚ÌŽæ“¾
 		const auto& moveAxis = INPUT_MANAGER.GetMoveAxis();
+		// “ü—Í‚ª‚ ‚é‚©‚Ç‚¤‚©
+		const bool hasInput = (moveAxis.x != 0.f || moveAxis.y != 0.f);
+
+		if (hasInput == false) return;
 
 		registry.view<ecs::PlayerMovementComponent, ecs::PlayerTag>().each(
-			[&](ecs::PlayerMovementComponent& movement)
+			[&](ecs::PlayerMovementComponent& movement, ecs::PlayerStateComponent& state)
 			{
 				movement.MoveInput = { moveAxis.x, 0.f, moveAxis.y };
+				state.AddRequest(ePlayerState::Move, 1);
 			});
 	}
 }
--- App/src/system/Player/State/PlayerStateSystem.cpp
+++ App/src/system/Player/State/PlayerStateSystem.cpp
@@ -18,14 +18,21 @@ namespace ecs
 	{
 		if (state.Requests.empty()) return;
 
+		// リクエストの優先度に基づいてソート
 		std::sort(state.Requests.begin(), state.Requests.end(),
 			[](const ecs::PlayerStateRequest& a, const ecs::PlayerStateRequest& b)
 			{
 				return a.Priority > b.Priority;
 			});
 
+		// 優先度の高いリクエストから順に処理
 		for (const auto& request : state.Requests)
 		{
+			if (state.CurrentState == request.State)
+			{
+				break;
+			}
+
 			if (state.CanTransition(request.State))
 			{
 				state.CurrentState = request.State;
